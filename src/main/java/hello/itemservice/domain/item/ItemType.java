e7e68621bfffb351cb919f60e992e6a5f9709268
Kwon seong jun <tjdwns1865@naver.com>
2022-04-04 18:55:47 +0900

    ItemType 오타 수정

--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -2,7 +2,7 @@ package hello.itemservice.domain.item;
 
 public enum ItemType {
 
-    BOOK("도서"), FODD("식품"), ETC("기타");
+    BOOK("도서"), FOOD("식품"), ETC("기타");
 
     private final String description;
 
